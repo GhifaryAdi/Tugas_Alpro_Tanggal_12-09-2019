a322ed2d59df67ee1e810bbc3a61cb8f5297c544
GhifaryAdi <54666049+GhifaryAdi@users.noreply.github.com>
2019-09-12 08:46:21 +0700

    Ghifary Adinegoro
    1201181238
    TI-42-11
    
    Tugas Alpro

---
+++ Tugas Alpro Tanggal 12-09-2019.py
@@ -0,0 +1,15 @@
+print("Start")
+GPA = [3, 2.7, 2.5, 4]
+
+def perkalian():
+    Total = index * Bonus
+    return Total
+
+Bonus = 500000
+
+for index in GPA:
+    print(index)
+    if index > 3:
+        print("Anda mendapatkan bonus = Rp.", perkalian())
+    elif index <= 3:
+        print("Tidak ada bonus")
